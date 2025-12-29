d376cb238fe068b5a210ccb0a720f6a944b28f7e
Keshav Gupta <gupta.keshav0406@email.com>
2025-12-29 19:46:20 +0530

    Redis setup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,111 +1,174 @@
-const userModel=require("../models/user.model")
-const jwt=require("jsonwebtoken")
-const bcrypt=require("bcryptjs")
+const userModel = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const Redis = require("../db/redis")
 
-async function registerUser(req,res){
+async function registerUser(req, res) {
 
-   try{
-     const {email,fullName:{firstName,lastName},password}=req.body
+    try {
+        const { email, fullName: { firstName, lastName }, password } = req.body
 
-    const isUserExist=await userModel.findOne({
-        email
-    })
+        const isUserExist = await userModel.findOne({
+            email
+        })
 
-    if(isUserExist){
-        return res.status(409).json({
-            message:"User already exist"
+        if (isUserExist) {
+            return res.status(409).json({
+                message: "User already exist"
+            })
+        }
+
+
+
+        const hashedPass = await bcrypt.hash(password, 10)
+
+        const User = await userModel.create({
+            email,
+            fullName: {
+                firstName,
+                lastName
+            },
+            password: hashedPass
+
+        })
+
+        const token = jwt.sign({
+            id: User._id,
+            email: User.email
+        }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" })
+
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 20 * 60 * 60 * 1000
+        })
+
+        res.status(201).json({
+            message: "User registered successfully",
+            User
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Error while registering User",
+            Error: err.message
         })
     }
 
-    
+}
 
-    const hashedPass=await bcrypt.hash(password,10)
+async function loginUser(req, res) {
 
-     const User=await userModel.create({
-        email,
-        fullName:{
-            firstName,
-            lastName
-        },
-        password:hashedPass
+    try {
+        const { email, password } = req.body
+
+        const User = await userModel.findOne({
+            email
+        }).select("+password")
+
+        if (!User) {
+            return res.status(400).json({
+                message: "User is not registred"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, User.password)
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Password "
+            })
+        }
+
+        const token = jwt.sign({
+            id: User._id,
+            email: User.email
+        }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" })
+
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 20 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({
+            message: "User logged in successfully",
+            User
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Failed to Login",
+            error: err.message
+        })
+    }
+}
+
+async function logoutUser(req, res) {
+
+    try {
+        const { token } = req.cookies.token
+
+        if (token) {
+            await Redis.set(`blacklist:${token}`, "true", "EX", 24 * 60 * 60)
+        }
+
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true
+        })
+
+        return res.status(200).json({ message: "logout successfully" })
+
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "Error while logging out",
+            error: err.message
+        })
+    }
+}
+
+async function getme(req, res) {
+
+    try {
+        const { token } = req.cookies.token
+
+        if (!token) {
+            return res.status(400).json({ message: "Token does not exists", authenticated: false })
+        }
+
+        const isBlacklisted = Redis.get(`blacklist:${token}`)
+
+        if (isBlacklisted) {
+            return res.status(400).json({
+                message: "token is blacklisted",
+                authenticated: false
+            })
+        }
+
+         let decoded;
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        } catch (jwtErr) {
+            // This will catch expired or invalid token
+            return res.status(401).json({ authenticated: false, message: jwtErr.message });
+        }
        
-    })
 
-    const token=jwt.sign({
-        id:User._id,
-        email:User.email
-    },process.env.JWT_SECRET_KEY,{expiresIn:"1d"})
+        return res.status(200).json({authenticated:true})
 
-
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none",
-        maxAge:20*60*60*1000
-    })
-
-    res.status(201).json({
-        message:"User registered successfully",
-        User
-    })
-
-}catch(err){
-    res.status(500).json({
-        message:"Error while registering User",
-        Error:err.message
-    })
-}
-    
-}
-
-async function loginUser(req,res){
-
-   try{
-     const{email,password}=req.body
-
-    const User=await userModel.findOne({
-        email
-    }).select("+password")
-
-    if(!User){
-        return res.status(400).json({
-            message:"User is not registred"
+    } catch (err) {
+        return res.status(500).json({
+            message:"Error while verifying",
+            error:err.message
         })
     }
-
-    const isMatch=await bcrypt.compare(password,User.password)
-
-    if(!isMatch){
-        return res.status(400).json({
-            message:"Invalid Password "
-        })
-    }
-
-     const token=jwt.sign({
-        id:User._id,
-        email:User.email
-    },process.env.JWT_SECRET_KEY,{expiresIn:"1d"})
-
-
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none",
-        maxAge:20*60*60*1000
-    })
-
-    return res.status(200).json({
-        message:"User logged in successfully",
-        User
-    })
-
-}catch(err){
-    return res.status(500).json({
-        message:"Failed to Login",
-        error:err.message
-    })
-}
 }
 
 
-module.exports={registerUser,loginUser}
+module.exports = { registerUser, loginUser, logoutUser, getMe}
