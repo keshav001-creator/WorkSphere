936716571c491e64f7df176750db1b5cbb51d938
Keshav Gupta <gupta.keshav0406@email.com>
2026-01-04 03:41:52 +0530

    Remove old root-level src and config files

--- src/routes/notification.routes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const {authUser}=require("../middlewares/auth.middleware")
-const getNotifications=require("../controllers/notification.controller")
-const router=express.Router()
-
-router.get("/notifications",authUser,getNotifications)
-
-module.exports=router
